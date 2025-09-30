d2406b9f5c56622b8a15dfaf20daa46f8ec6f961
zhangwenjin <zwjhb12@163.com>
2025-09-30 10:39:19 +0800

    feat: add compodoc for project.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,19 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersPrarmDto } from './dtos/get-users-prarm.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 
+/**
+ * Class to connect to Users table and perform business operations
+ */
 @Injectable()
 export class UsersService {
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+
+  /**
+   * The method to get all the users from the database
+   */
   public findAll(
     getUsersPrarmDto: GetUsersPrarmDto,
     limit: number,
@@ -28,6 +35,9 @@ export class UsersService {
     ];
   }
 
+  /**
+   * Find a Single user using the ID of the user
+   */
   public findOneById(id: string) {
     return {
       id: 123,
