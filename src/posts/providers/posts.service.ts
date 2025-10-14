4277906588703a3e68215aeda0d9f200d572d8c4
zhangwenjin <zwjhb12@163.com>
2025-10-14 17:08:54 +0800

    feat: post module'supdate method and delete test.

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MetaOption } from 'src/meta-options/meta-option.entity';
 import { TagsService } from 'src/tags/providers/tags.service';
+import { PatchPostDto } from '../dtos/patch-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -104,5 +105,34 @@ export class PostsService {
     // 6.15
     await this.postsRepository.delete(id);
     return { deleted: true, id };
+  }
+
+  public async update(patchPostDto: PatchPostDto) {
+    // find the post
+    let post = await this.postsRepository.findOneBy({
+      id: patchPostDto.id,
+    });
+
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    // find the tags only if tags are provided
+    if (patchPostDto.tags) {
+      const tags = await this.tagsService.findMultipleTags(patchPostDto.tags);
+      post.tags = tags;
+    }
+
+    // update the properties
+    post.title = patchPostDto.title ?? post.title;
+    post.content = patchPostDto.content ?? post.content;
+    post.status = patchPostDto.status ?? post.status;
+    post.postType = patchPostDto.postType ?? post.postType;
+    post.slug = patchPostDto.slug ?? post.slug;
+    post.featuredImage = patchPostDto.featuredImage ?? post.featuredImage;
+    post.publishOn = patchPostDto.publishOn ?? post.publishOn;
+
+    // save the post and return
+    return await this.postsRepository.save(post);
   }
 }
