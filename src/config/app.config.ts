a02479a3770a2bfb445587e52c531f9875f2b211
zhangwenjin <zwjhb12@163.com>
2025-10-19 16:46:21 +0800

    feat: 16 serialization and interceptors.

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,5 @@
 import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => ({
   environment: process.env.NODE_ENV || 'production',
+  apiVersoin: process.env.API_VERSION,
 }));
